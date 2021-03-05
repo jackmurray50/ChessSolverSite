2d1bdff1901b0a5672ffbad10a2729b759462100
jackmurray50 <jack.murray50@gmail.com>
2021-03-04 22:19:44 -0500

    Create BoardRelationshipModel.cs

---
+++ chess solver site/Models/BoardRelationshipModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chess_solver_site.Models
+{
+    public class BoardRelationshipModel
+    {
+    }
+}
