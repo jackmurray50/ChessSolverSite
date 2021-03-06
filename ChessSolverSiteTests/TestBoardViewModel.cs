429444f74d2b1884779004d0c04347412961913b
jackmurray50 <jack.murray50@gmail.com>
2021-03-06 11:03:17 -0500

    Created and tested BoardViewModel

---
+++ ChessSolverSiteTests/TestBoardViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+using chess_solver_site.Models;
+
+namespace ChessSolverSiteTests
+{
+    public class TestBoardViewModel
+    {
+        [Fact]
+        public void Test_Add()
+        {
+            BoardViewModel bm = new BoardViewModel();
+            bm.Turn = "BLACK";
+            bm.BoardState = "Lah lah lah";
+            bm.TurnsSinceCapture = 9;
+            bm.WinState = "TBD";
+            bm.Add();
+            Assert.True(bm.Id > 0);
+        }
+        [Fact]
+        public void Test_GetById()
+        {
+
+        }
+    }
+}
---
+++ chess solver site/Models/BoardViewModel.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace chess_solver_site.Models
+{
+    public class BoardViewModel
+    {
+        private BoardModel _model;
+
+        public int Id;
+        public string BoardState;
+        public int TurnsSinceCapture;
+        public string Turn;
+        public string WinState;
+
+        public BoardViewModel()
+        {
+            _model = new BoardModel();
+        }
+
+
+        public void GetById()
+        {
+            try
+            {
+                Boards board = _model.GetById(Id);
+
+                Id = board.Id;
+                BoardState = board.BoardState;
+                Turn = board.Turn;
+                TurnsSinceCapture = board.TurnsSinceCapture;
+                WinState = board.WinState;
+            }
+            catch (NullReferenceException)
+            {
+                BoardState = "not found";
+            }
+            catch (Exception ex)
+            {
+                BoardState = "not found";
+                Console.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod().Name + " " + ex.Message);
+                throw ex;
+            }
+        }
+    
+        public void GetByBoardState()
+        {
+            try
+            {
+                Boards board = _model.GetByBoardState(BoardState);
+
+                Id = board.Id;
+                BoardState = board.BoardState;
+                Turn = board.Turn;
+                TurnsSinceCapture = board.TurnsSinceCapture;
+                WinState = board.WinState;
+            }
+            catch (NullReferenceException)
+            {
+                BoardState = "not found";
+            }
+            catch (Exception ex)
+            {
+                BoardState = "not found";
+                Console.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod().Name + " " + ex.Message);
+                throw ex;
+            }
+        }
+    
+        public void Add()
+        {
+            try
+            {
+                Id = -1;
+                Boards board = new Boards();
+                board.BoardState = BoardState;
+                board.Turn = Turn;
+                board.TurnsSinceCapture = TurnsSinceCapture;
+                board.WinState = WinState;
+                Id = _model.Add(board);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod().Name + " " + ex.Message);
+                throw ex;
+            }
+        }
+    
+        public int Update()
+        {
+            UpdateStatus opStatus = UpdateStatus.Failed;
+            try
+            {
+                Id = -1;
+                Boards board = new Boards();
+                board.BoardState = BoardState;
+                board.Turn = Turn;
+                board.TurnsSinceCapture = TurnsSinceCapture;
+                board.WinState = WinState;
+                opStatus = _model.Update(board);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod().Name + " " + ex.Message);
+                throw ex;
+            }
+            return Convert.ToInt16(opStatus);
+        }
+
+        public int Delete()
+        {
+            int boardDeleted = -1;
+
+            try
+            {
+                boardDeleted = _model.Delete(Id);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod().Name + " " + ex.Message);
+                throw ex;
+            }
+            return boardDeleted;
+        }
+    }
+}
