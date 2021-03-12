37dc09b1f2b698d8e3f8c8af528ad26b385416da
jackmurray50 <jack.murray50@gmail.com>
2021-03-12 09:41:17 -0500

    Added BoardViewModel to allow for Json deserialization of server-side BoardViewModels

---
+++ chess solver client/BoardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace chess_solver_client
+{
+    class BoardViewModel
+    {
+        public int Id { get; set; }
+        public string BoardState { get; set; }
+        public int TurnsSinceCapture { get; set; }
+        public string Turn { get; set; }
+        public string WinState { get; set; }
+        public int VerificationAmount { get; set; }
+    }
+}
