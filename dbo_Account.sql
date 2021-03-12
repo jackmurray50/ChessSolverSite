b858ea61e104f1dff2d6115e61d3054e68f0bc15
jackmurray50 <jack.murray50@gmail.com>
2021-03-12 07:35:16 -0500

    Delete dbo_Account.sql

--- dbo_Account.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Account]
-(
-	[Id] INT NOT NULL PRIMARY KEY,
-	[Password] CHAR(64), --Can be null, as a password won't be required.
-	[Name] VARCHAR(32) NOT NULL,
-	[Progress] BIGINT NOT NULL
-)
